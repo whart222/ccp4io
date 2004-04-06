4c1c4c8ca5415ed292498a36347b9c9212dc2cce
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2004-04-06 01:10:31 +0000

    bug fix in mat4_to_symop

--- lib/src/ccp4_parser.c
+++ lib/src/ccp4_parser.c
@@ -1660,7 +1660,12 @@ char *mat4_to_symop(char *symchs_begin, char *symchs_end, const float rsm[4][4])
 	    ccp4_signal(CCP4_ERRLEVEL(3) | CPARSER_ERRNO(CPARSERR_MatToSymop), 
 			"mat4_to_symop 2", NULL);
 	    return NULL; }
-	  *ich++ = '-';
+          if (jdo10 != 3) {
+            *ich++ = '-';
+          }
+          else {
+            *ich++ = '+'; // because of "while (itr < 0) itr += 12;" below
+          }
           ist = 1;
 	}
       
